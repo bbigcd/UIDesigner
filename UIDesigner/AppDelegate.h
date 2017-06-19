dd8625c4c5adf79ff4e8f53fae84b6fae76594a7
bbigcd <bbigcd@163.com>
2017-06-19 21:20:53 +0800

    Initial Commit

---
+++ UIDesigner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIDesigner
+//
+//  Created by chendi on 2017/6/19.
+//  Copyright © 2017年 chendi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIDesigner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIDesigner
+//
+//  Created by chendi on 2017/6/19.
+//  Copyright © 2017年 chendi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
